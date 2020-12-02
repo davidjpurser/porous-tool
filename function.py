f70f6a91aa777feb61a1101264c9f5fc41fdf5d7
David Purser <dpurser@mpi-sws.org>
2020-12-02 19:04:20 +0000

    handle two pure inverters

--- function.py
+++ function.py
@@ -9,7 +9,11 @@ class function:
 		self.add = a
 
 	def __repr__(self):
-		return "f(x) = {}x + {}".format(self.mult,self.add)
+		if self.mult ==1 :
+			txt = ""
+		else:
+			txt = self.mult
+		return "f(x) = {}x + {}".format(txt,self.add)
 
 	def setMult(self,mult):
 		self.mult = mult
@@ -31,6 +35,9 @@ class function:
 	def isNegativeCounter(self):
 		return self.isCounter() and self.add < 0
 
+	def isPureInverter(self):
+		return self.mult == -1
+
 	def isInverter(self):
 		return self.mult < 0
 
@@ -40,4 +47,19 @@ class function:
 	def growingFrom(self):
 		#only makes sense if not counter
 		return int(abs(self.add)/(abs(self.mult)-1))
+
+	def isEqual(self, fun):
+		return self.mult == fun.mult and self.add == fun.add
+
+	def compose(self, fun):
+		a = self.mult
+		b = self.add
+		c = fun.mult
+		d = fun.add
+		return function(a*c, a*d + b)
+
+	def isIdentity(self):
+		return self.isEqual(function(1,0))
+
+
 
--- setup.py
+++ setup.py
@@ -12,62 +12,77 @@ def lgcd(list):
 
 functions = [
 	function(3,11),
-	function(-2,0),
-	function(1,33),
-	function(1,-11)
+	function(1,3),
+	function(1,-43),
 ]
 print(functions)
-
 x = 1
-x0 = set([x])
 target = 8
+def compute(x,target, functions):
+	# strip useless identity function
+	functions = [x for x in functions if not x.isIdentity()]
+	print(functions)
+	x0 = set([x])
+	growStatus = [x.isGrower() for x in functions]
+	positiveCounter = [x.isPositiveCounter() for x in functions]
+	negativeCounter = [x.isNegativeCounter() for x in functions]
+	pureInverters = [x.isPureInverter() for x in functions]
 
-growStatus = [x.isGrower() for x in functions]
-positiveCounter = [x.isPositiveCounter() for x in functions]
-negativeCounter = [x.isNegativeCounter() for x in functions]
+	# multiple pure inverters, then there are bi-directional counters
+	if not any(positiveCounter) and not any(negativeCounter) and sum(pureInverters) > 1:
+		inverters = [x for x in functions if x.isPureInverter()]
+		a = inverters[0].compose(inverters[1])
+		b = inverters[1].compose(inverters[0])
+		print("there are counters! in both directions", a,b)
+		print("adding both", a,b)
+		functionsplus = functions + [a,b]
+		return compute(x,target, functionsplus)
 
-allGrowers = all(growStatus)
-print(growStatus, allGrowers)
+	if all(growStatus):
+		semi = semilinear()
+		growPoints =[x.growingFrom() for x in functions]
+		growPoint = max(growPoints)
+		growPoint = max(growPoint, target+2)
+		print(growPoints, growPoint)
+		start = saturateTo(x0, functions, growPoint)
+		semi.addPoints(start)
+		dir1 = linearset(growPoint,1,1)
+		dir2 = linearset(-growPoint,-1,1)
+		semi.add(dir1)
+		semi.add(dir2)
+		print(semi)
+		return semi
 
-if allGrowers:
-	semi = semilinear()
-	growPoints =[x.growingFrom() for x in functions]
-	growPoint = max(growPoints)
-	growPoint = max(growPoint, target+2)
-	print(growPoints, growPoint)
-	start = saturateTo(x0, functions, growPoint)
-	semi.addPoints(start)
-	dir1 = linearset(growPoint,1,1)
-	dir2 = linearset(-growPoint,-1,1)
-	semi.add(dir1)
-	semi.add(dir2)
-	print(semi)
+	if any(positiveCounter) and any(negativeCounter):
+		counters = [x for x in functions if x.isCounter()]
+		amounts = [x.getCounterAmount() for x in counters]
+		print(amounts)
+		period = lgcd(amounts)
+		print(period)
+		semi = semilinear()
+		lset1 = linearset(x,period,-1)
+		semi.add(lset1)
+		Q = [lset1]
+		while len(Q) > 0 :
+			top = Q.pop()
+			for f in functions:
+				b = top.getBase()
+				next = f.apply(b)
+				myset = linearset(next,period,-1)
+				print(next, linearset(next,period,-1))
+				if not semi.contains(myset):
 
-if any(positiveCounter) and any(negativeCounter):
-	counters = [x for x in functions if x.isCounter()]
-	amounts = [x.getCounterAmount() for x in counters]
-	print(amounts)
-	period = lgcd(amounts)
-	print(period)
-	semi = semilinear()
-	lset1 = linearset(x,period,-1)
-	semi.add(lset1)
-	Q = [lset1]
-	while len(Q) > 0 :
-		top = Q.pop()
-		for f in functions:
-			b = top.getBase()
-			next = f.apply(b)
-			myset = linearset(next,period,-1)
-			print(next, linearset(next,period,-1))
-			if not semi.contains(myset):
+					semi.add(myset)
+					Q.append(myset)
+		print(semi)
+		return semi
 
-				semi.add(myset)
-				Q.append(myset)
-	print(semi)
+	print("I don't know how to handle this case")
+	return None
 
-# print(f1.apply(4))
-# # for _ in range(2):
-# # 	x0 = applySet(x0, functions)
-# # 	print(x0)
-# print(saturateTo(x0, functions, 100))
+compute(x,target,functions)
+	# print(f1.apply(4))
+	# # for _ in range(2):
+	# # 	x0 = applySet(x0, functions)
+	# # 	print(x0)
+	# print(saturateTo(x0, functions, 100))
